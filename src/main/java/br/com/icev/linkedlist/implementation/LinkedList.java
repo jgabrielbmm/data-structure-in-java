965a0cbac2755ad7075e0ff39fa5b9fa46c862e4
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2023-04-13 17:58:07 -0300

    Implementation: linked list add method creation

--- src/main/java/br/com/icev/linkedlist/implementation/LinkedList.java
+++ src/main/java/br/com/icev/linkedlist/implementation/LinkedList.java
@@ -5,6 +5,52 @@ public class LinkedList<T> {
     private Node<T> tail;
     private Integer size = 0;
 
+    private LinkedList<T> list = new LinkedList<>();
+
+    private void addInHead(T value){
+        Node <T> cell = new Node<>(value);
+        if (size == 0) {
+            head = cell;
+            tail = cell;
+            size++;
+            return;
+        }
+
+        cell.setNext(cell);
+        head = cell;
+        size++;
+    }
+
+    private void addInTail(T value) {
+        Node <T> cell = new Node<>(value);
+        tail.setNext(cell);
+        tail = cell;
+        size++;
+    }
+
+    private void addInTheMiddle(T value, int position){
+        Node<T> cell = new Node<>(value);
+        Node<T> aux = head;
+        for(int i = 0; i < position; i++){
+            aux = aux.getNext();
+        }
+        cell.setNext(aux.getNext());
+        aux.setNext(cell);
+        size++;
+    }
+
+    public void add (T value, int position) {
+        if(position == 0 || size == 0){
+            list.addInHead(value);
+        } else if (position == size) {
+            list.addInTail(value);
+        } else if (position > 0 && position < size) {
+            list.addInTheMiddle(value, position);
+        } else {
+            System.out.println("");
+        }
+    }
+
     public Integer getSize() {
         return size;
     }
--- src/main/java/br/com/icev/linkedlist/implementation/Node.java
+++ src/main/java/br/com/icev/linkedlist/implementation/Node.java
@@ -4,6 +4,10 @@ public class Node <T> {
     private T value;
     private Node<T> next;
 
+    public Node(T value) {
+        this.value = value;
+    }
+
     public T getValue() {
         return value;
     }
