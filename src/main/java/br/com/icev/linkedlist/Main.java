8a003ac08535b36c8f79faff2633ad1d20f435d0
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2023-04-15 14:54:36 -0300

    Add function correction and creating error message when user try set a new value out of range

--- src/main/java/br/com/icev/linkedlist/Main.java
+++ src/main/java/br/com/icev/linkedlist/Main.java
@@ -1,4 +1,19 @@
 package br.com.icev.linkedlist;
 
+import br.com.icev.linkedlist.implementation.LinkedList;
+import br.com.icev.linkedlist.implementation.Node;
+
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1, 0);
+        list.add(4, 1);
+        list.add(7, 2);
+        list.add(11, 0);
+        list.add(65, 2);
+        System.out.println(list.getSize());
+        System.out.println(list);
+    }
 }
--- src/main/java/br/com/icev/linkedlist/implementation/LinkedList.java
+++ src/main/java/br/com/icev/linkedlist/implementation/LinkedList.java
@@ -5,7 +5,11 @@ public class LinkedList<T> {
     private Node<T> tail;
     private Integer size = 0;
 
-    private LinkedList<T> list = new LinkedList<>();
+    private void CheckIndexOfRange(Integer index){
+        if (index > size || index < 0){
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+        }
+    }
 
     private void addInHead(T value){
         Node <T> cell = new Node<>(value);
@@ -15,8 +19,7 @@ public class LinkedList<T> {
             size++;
             return;
         }
-
-        cell.setNext(cell);
+        cell.setNext(head);
         head = cell;
         size++;
     }
@@ -31,7 +34,7 @@ public class LinkedList<T> {
     private void addInTheMiddle(T value, int position){
         Node<T> cell = new Node<>(value);
         Node<T> aux = head;
-        for(int i = 0; i < position; i++){
+        for(int i = 0; i < position -1; i++){
             aux = aux.getNext();
         }
         cell.setNext(aux.getNext());
@@ -40,18 +43,49 @@ public class LinkedList<T> {
     }
 
     public void add (T value, int position) {
-        if(position == 0 || size == 0){
-            list.addInHead(value);
-        } else if (position == size) {
-            list.addInTail(value);
-        } else if (position > 0 && position < size) {
-            list.addInTheMiddle(value, position);
-        } else {
-            System.out.println("");
+        CheckIndexOfRange(position);
+        if(position == 0){
+            addInHead(value);
+            System.out.println("Entrei no 0");
         }
+        else if( position == size){
+            addInTail(value);
+            System.out.println("Entrei no 1");
+        }
+        else{
+            addInTheMiddle(value, position);
+            System.out.println("Entrei no 2");
+        }
+
     }
 
+//    public void push(int position){
+//        for(T current : list){
+//            System.out.println(current);
+//        }
+//    }
     public Integer getSize() {
         return size;
+    }
+
+    @Override
+    public String toString() {
+
+        if(size == 0) {
+            return "[]";
+        }
+
+        StringBuilder builder = new StringBuilder("[");
+
+        Node<T> current = head;
+
+        for (int i = 0; i < size - 1; i++) {
+            builder.append(current.getValue()).append(",");
+            current = current.getNext();
+        }
+
+        builder.append(current.getValue()).append("]");
+
+        return builder.toString();
     }
 }
